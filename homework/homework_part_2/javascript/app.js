81854af411ebd7c6bc403b5cc0f4ca4a62a7c6e4
Josh Hepworth <jrhepworth12@gmail.com>
2018-09-11 15:43:02 -0500

    added first response

---
+++ homework/homework_part_2/javascript/app.js
@@ -0,0 +1,9 @@
+console.log('Welcome');
+
+// Question 1. The difference between an Interpolation is using 
+// variables in a console.log vs not using them.
+
+// Concatenation - console.log('Hello' + ' Good' + ' Morning');
+
+// const word = "wonderful";
+// Interpolation - console.log('What a ' + word + ' day');
